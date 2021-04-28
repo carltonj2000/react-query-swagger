3933198f9316fb127b977978c11e1af8c5a8c393
Carlton Joseph <carlton.joseph@gmail.com>
2021-04-27 20:18:11 -0700

    add minimal swagger definitions

--- server/Routes/posts.js
+++ server/Routes/posts.js
@@ -1,5 +1,42 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Post:
+ *       type: object
+ *       required:
+ *         - userId
+ *         - title
+ *         - body
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The Auto-generated id of a post
+ *         userId:
+ *           type: integer
+ *           description: id of author
+ *         title:
+ *           type: string
+ *           description: title of post
+ *         body:
+ *           type: string
+ *           description: content of post *
+ *       example:
+ *         id: 1
+ *         userId: 1
+ *         title: my title
+ *         body: my article
+ *
+ */
+
+/**
+ * @swagger
+ *  tags:
+ *    name: Posts
+ *    description: posts of users
+ */
+
 import express from "express";
-import bodyParser from "body-parser";
 import data from "../data";
 
 const postRouter = express.Router();
